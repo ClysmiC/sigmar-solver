034b29671c0a2a7a566267524c9d92842a754711
ClysmiC <asmith379@gatech.edu>
2017-10-22 22:38:00 -0700

    can compute which tiles are active

--- solver.py
+++ solver.py
@@ -1,7 +1,8 @@
 from PIL import Image
+from enum import IntEnum
 
 
-# Coordinates for hex grids are tricky. My favorite way to represent them is below:
+# Coordinates for hex grids are tricky. A convenient way to represent them is below:
 #
 # Y
 #  \
@@ -20,30 +21,70 @@ from PIL import Image
 #           \________/
 #      (0, 0)       (5, 0)
 
-blank = 0
-air = 1
-fire = 2
-salt = 3
-water = 4
-vitae = 5
-earth = 6
-mors = 7
-quicksilver = 8
-lead = 9
-tin = 10
-iron = 11
-copper = 12
-silver = 13
-gold = 14
+# I number the neighbors of a hex as follows:
+#
+#          
+#     5   / \   0
+#        /   \
+#    4  |     |  1
+#       |     |
+#        \   /
+#     3   \ /  2
 
-elements = ["blank", "air", "fire", "salt", "water", "vitae", "earth", "mors", "quicksilver", "lead", "tin", "iron", "copper", "silver", "gold"]
+class Tile(IntEnum):
+    BLANK = 0
+    AIR = 1
+    FIRE = 2
+    SALT = 3
+    WATER = 4
+    VITAE = 5
+    EARTH = 6
+    MORS = 7
+    QUICKSILVER = 8
+    LEAD = 9
+    TIN = 10
+    IRON = 11
+    COPPER = 12
+    SILVER = 13
+    GOLD = 14
+    
+class GameState:
+    def __init__(self, board, currentMetal=Tile.LEAD):
+        self.board = board
+        self.currentMetal = currentMetal
+
+elements = ["BLANK", "AIR", "FIRE", "SALT", "WATER", "VITAE", "EARTH", "MORS", "QUICKSILVER", "LEAD", "TIN", "IRON", "COPPER", "SILVER", "GOLD"]
 initialCounts = [36, 8, 8, 4, 8, 4, 8, 4, 5, 1, 1, 1, 1, 1, 1]
 
-def elementName(element):
-    if element is None:
+def tileAt(xy, board):
+    if xy[0] < 0 or xy[0] > 10 or xy[1] < 0 or xy [1] > 10:
+        return None
+    else:
+        return board[xy[1]][xy[0]]
+    
+def isBlankOrNone(xy, board):
+    return not isElement(xy, board)
+
+def isElement(xy, board):
+    tile = tileAt(xy, board)
+    return tile is not None and tile > Tile.BLANK
+
+def isMetal(xy, board):
+    return tileAt(xy, board) >= Tile.LEAD
+
+def isMetalThatIsntCurrent(xy, state):
+    board = state.board
+    
+    if isMetal(xy, board):
+        return tileAt(xy, board) != state.currentMetal
+
+    return False
+
+def tileName(tile):
+    if tile is None:
         return "None"
     
-    return elements[element]
+    return elements[tile]
 
 def exitWithError(error):
     print(error)
@@ -51,70 +92,60 @@ def exitWithError(error):
 
 def solve():
 
-    colors = []
+    # colors = []
+    # board = [[None] * 11] * 11
 
-    # board = [[None] * 10] * 10
+    # TODO: replace hard-coded board with mechanism to read board in from the screen
     board = [ \
-              [vitae, blank, blank, mors, blank, blank, None, None, None, None, None], \
-              [blank, mors, blank, tin, fire, fire, air, None, None, None, None], \
-              [blank, blank, iron, quicksilver, blank, water, quicksilver, blank, None, None, None], \
-              [blank, blank, air, earth, blank, fire, salt, blank, blank, None, None], \
-              [salt, vitae, water, air, earth, air, quicksilver, water, blank, blank, None], \
-              [blank, earth, water, earth, water, gold, water, air, earth, water, silver], \
-              [None, blank, quicksilver, blank, blank, water, vitae, blank, fire, blank, blank], \
-              [None, None, quicksilver, fire, copper, earth, lead, earth, blank, air, blank], \
-              [None, None, None, blank, blank, salt, salt, air, fire, air, fire], \
-              [None, None, None, None, blank, mors, blank, blank, mors, fire, blank], \
-              [None, None, None, None, None, earth, blank, blank, blank, vitae, blank] \
+              [Tile.VITAE, Tile.BLANK, Tile.BLANK, Tile.MORS, Tile.BLANK, Tile.BLANK, None, None, None, None, None], \
+              [Tile.BLANK, Tile.MORS, Tile.BLANK, Tile.TIN, Tile.FIRE, Tile.FIRE, Tile.AIR, None, None, None, None], \
+              [Tile.BLANK, Tile.BLANK, Tile.IRON, Tile.QUICKSILVER, Tile.BLANK, Tile.WATER, Tile.QUICKSILVER, Tile.BLANK, None, None, None], \
+              [Tile.BLANK, Tile.BLANK, Tile.AIR, Tile.EARTH, Tile.BLANK, Tile.FIRE, Tile.SALT, Tile.BLANK, Tile.BLANK, None, None], \
+              [Tile.SALT, Tile.VITAE, Tile.WATER, Tile.AIR, Tile.EARTH, Tile.AIR, Tile.QUICKSILVER, Tile.WATER, Tile.BLANK, Tile.BLANK, None], \
+              [Tile.BLANK, Tile.EARTH, Tile.WATER, Tile.EARTH, Tile.WATER, Tile.GOLD, Tile.WATER, Tile.AIR, Tile.EARTH, Tile.WATER, Tile.SILVER], \
+              [None, Tile.BLANK, Tile.QUICKSILVER, Tile.BLANK, Tile.BLANK, Tile.WATER, Tile.VITAE, Tile.BLANK, Tile.FIRE, Tile.BLANK, Tile.BLANK], \
+              [None, None, Tile.QUICKSILVER, Tile.FIRE, Tile.COPPER, Tile.EARTH, Tile.LEAD, Tile.EARTH, Tile.BLANK, Tile.AIR, Tile.BLANK], \
+              [None, None, None, Tile.BLANK, Tile.BLANK, Tile.SALT, Tile.SALT, Tile.AIR, Tile.FIRE, Tile.AIR, Tile.FIRE], \
+              [None, None, None, None, Tile.BLANK, Tile.MORS, Tile.BLANK, Tile.BLANK, Tile.MORS, Tile.FIRE, Tile.BLANK], \
+              [None, None, None, None, None, Tile.EARTH, Tile.BLANK, Tile.BLANK, Tile.BLANK, Tile.VITAE, Tile.BLANK] \
             ]
 
     verifyInput(board)
 
+    state = GameState(board)
+
     activeTiles = []
 
-    topLeft = (810, 200)
-    middle = (980, 485)
-    bottomRight = (1140, 770)
+    for y in range(len(board)):
+        for x in range(len(board[y])):
 
-    wSample1 = (middle[0] - topLeft[0]) / 2.5
-    wSample2 = (bottomRight[0] - middle[0]) / 2.5
-    wSample3 = (bottomRight[0] - topLeft[0]) / 5
+            if isElement((x, y), board) and isActive((x, y), state):
+                activeTiles.append((x, y))
 
-    hSample1 = (middle[1] - topLeft[1]) / 5
-    hSample2 = (bottomRight[1] - middle[1]) / 5
-    hSample3 = (bottomRight[1] - topLeft[1]) / 10
-
-    # (x, y) in screen-space needed to traverse one x or y step in grid-space
-    xStep = ((wSample1 + wSample2 + wSample3) / 3 , 0)
-    yStep = (-xStep[0] / 2, -(hSample1 + hSample2 + hSample3) / 3)
-
-    print("x step = " + str(xStep))
-    print("y step = " + str(yStep))
-
-    for x in range(0, 10):
-        for y in range(0, 10):
-            if abs(x - y) >= 5:
-                continue
-
-    img = Image.open("input.png")
-    color = img.getpixel((810, 200))
-
-    print(color)
+    print(activeTiles)
 
 
 def verifyInput(board):
-    counts = [0] * (gold + 1)
-    
-    for x in range(0, 11):
-        for y in range(0, 11):
 
-            tile = board[x][y]
+    if len(board) != 11:
+        exitWithError("Wrong board height: " + str(len(board)))
+
+    for i in range(len(board)):
+        if len(board[i]) != 11:
+            exitWithError("Wrong board with in row " + str(i) + ": " + str(len(board[i])))
+
+    counts = [0] * (len(Tile) + 1)
+    
+    for y in range(0, 11):
+        for x in range(0, 11):
+
+            tile = tileAt((x, y), board)
             
             tileExists = tile is not None
             tileShouldExist = abs(x - y) <= 5
             
             if tileExists != tileShouldExist:
-                exitWithError("Error, tile at " + str((x, y)) + " should " + ("" if tileShouldExist else "not ") + " exist. Found: " + str(elementName(tile)))
+                exitWithError("Error, tile at " + str((x, y)) + " should " + ("" if tileShouldExist else "not ") + " exist. Found: " + str(tileName(tile)))
 
             if tileExists:
                 counts[tile] += 1
@@ -122,7 +153,58 @@ def verifyInput(board):
 
     for i in range(len(elements)):
         if counts[i] != initialCounts[i]:
-            exitWithError("Expected " + str(initialCounts[i]) + " " + elementName(i) + " tiles, found " + str(counts[i]))
+            exitWithError("Expected " + str(initialCounts[i]) + " " + tileName(i) + " tiles, found " + str(counts[i]))
+
+
+def getNeighborCoords(xy, board):
+    neighbors = []
+    neighbors.append((xy[0] + 1, xy[1] + 1))
+    neighbors.append((xy[0] + 1, xy[1] + 0))
+    neighbors.append((xy[0] + 0, xy[1] - 1))
+    neighbors.append((xy[0] - 1, xy[1] - 1))
+    neighbors.append((xy[0] - 1, xy[1] + 0))
+    neighbors.append((xy[0] + 0, xy[1] + 1))
+    return neighbors
+    
+def isActive(xy, state):
+    board = state.board
+    
+    if isBlankOrNone(xy, board):
+        return False
+    
+    if isMetalThatIsntCurrent(xy, state):
+        return False
+
+    neighbors = getNeighborCoords(xy, board)
+        
+    pos0Free = False
+    pos1Free = False
+    consecutiveFree = 0
+    
+    for i, n in enumerate(neighbors):
+        isFree = isBlankOrNone(n, board)
+
+        if isFree:
+            consecutiveFree += 1
+            
+            if i == 0:
+                pos0Free = True
+            elif i == 1:
+                pos1Free = True
+                
+        else:
+            consecutiveFree = 0
+
+        if consecutiveFree == 3:
+            return True
+
+    if consecutiveFree == 1 and pos0Free and pos1Free:
+        return True
+
+    if consecutiveFree == 2 and pos0Free:
+        return True
+        
+    return False
             
 
 solve()
